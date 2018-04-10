c3f0f16ab622ec52d7ab65035cd301f7d251929c
Árpád Goretity <h2co3@h2co3.org>
2018-04-10 20:43:59 +0200

    Add basic derive functionality for `trait BsonSchema`

--- magnet_derive/src/lib.rs
+++ magnet_derive/src/lib.rs
@@ -25,3 +25,94 @@
                  cast_possible_wrap, cast_possible_truncation,
                  mutex_integer, mut_mut, items_after_statements,
                  print_stdout, mem_forget, maybe_infinite_iter))]
+
+#[macro_use]
+extern crate quote;
+extern crate syn;
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use syn::{ DeriveInput, Data, DataStruct, DataEnum, Fields, Field };
+use syn::token::Comma;
+use syn::punctuated::Punctuated;
+use quote::Tokens;
+
+/// Implements `BsonSchema` for a given type based on its
+/// recursively contained types in fields or variants.
+/// TODO(H2CO3): handle generics
+#[proc_macro_derive(BsonSchema)]
+pub fn derive_bson_schema(input: TokenStream) -> TokenStream {
+    let parsed_ast: DeriveInput = syn::parse(input).expect("couldn't parse derive input");
+    let type_name = parsed_ast.ident;
+    let impl_ast = match parsed_ast.data {
+        Data::Struct(s) => impl_bson_schema_struct(s),
+        Data::Enum(e) => impl_bson_schema_enum(e),
+        Data::Union(_) => panic!("`BsonSchema` can't be implemented for unions"),
+    };
+    let generated = quote! {
+        #[automatically_derived]
+        impl ::magnet_schema::BsonSchema for #type_name {
+            fn bson_schema() -> ::bson::Document {
+                #impl_ast
+            }
+        }
+    };
+
+    generated.into()
+}
+
+/// Implements `BsonSchema` for a `struct`.
+fn impl_bson_schema_struct(ast: DataStruct) -> Tokens {
+    match ast.fields {
+        Fields::Named(fields) => {
+            impl_bson_schema_regular_struct(fields.named)
+        },
+        Fields::Unnamed(fields) => {
+            impl_bson_schema_tuple_struct(fields.unnamed)
+        },
+        Fields::Unit => {
+            impl_bson_schema_unit_struct()
+        },
+    }
+}
+
+/// Implements `BsonSchema` for a regular `struct` with named fields.
+fn impl_bson_schema_regular_struct(fields: Punctuated<Field, Comma>) -> Tokens {
+    // TODO(H2CO3): figure out and handle `"id"`-vs.-`"_id"`!!!
+    // TODO(H2CO3): handle `serde(rename)`, `serde(rename_all)`, `serde(skip)`, etc.
+    let property_names = fields.iter().map(
+        |field| field.ident.as_ref().expect("no name for named field?!").as_ref()
+    );
+    let required_names = fields.iter().map(
+        |field| field.ident.as_ref().expect("no name for named field?!").as_ref()
+    );
+    let types = fields.iter().map(|field| &field.ty);
+
+    quote! {
+        doc! {
+            "type": "object",
+            "properties": { #(#property_names: #types::bson_schema(),)* },
+            "required": [ #(#required_names,)* ],
+            "additionalProperties": false,
+        }
+    }
+}
+
+/// Implements `BsonSchema` for a tuple `struct` with unnamed/numbered fields.
+/// TODO(H2CO3): implement me
+fn impl_bson_schema_tuple_struct(fields: Punctuated<Field, Comma>) -> Tokens {
+    unimplemented!()
+}
+
+/// Implements `BsonSchema` for a unit `struct` with no fields.
+fn impl_bson_schema_unit_struct() -> Tokens {
+    quote! {
+        doc!{ "type": "null" }
+    }
+}
+
+/// Implements `BsonSchema` for an `enum`.
+/// TODO(H2CO3): implement me
+fn impl_bson_schema_enum(ast: DataEnum) -> Tokens {
+    unimplemented!()
+}
