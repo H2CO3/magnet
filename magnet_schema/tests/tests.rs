faaa260b996480f94a2e46f14488ba3be14040cf
Árpád Goretity <h2co3@h2co3.org>
2018-04-19 11:09:38 +0200

    Unit tests: `UnorderedDocument` (meta-test), unit structs, newtype structs, tuple structs, named-field structs

--- magnet_schema/tests/tests.rs
+++ magnet_schema/tests/tests.rs
@@ -137,8 +137,14 @@ fn unordered_doc_equality() {
         "foo": "bar",
         "qux": 42,
         "key": [
-            null,
-            1337,
+            {
+                "inner_1": null,
+                "inner_2": 1337,
+            },
+            {
+                "inner_3": "value",
+                "inner_4": -42,
+            },
         ],
         "inner": {
             "one": false,
@@ -148,8 +154,14 @@ fn unordered_doc_equality() {
 
     let d2 = unord_doc! {
         "key": [
-            null,
-            1337,
+            {
+                "inner_2": 1337,
+                "inner_1": null,
+            },
+            {
+                "inner_4": -42,
+                "inner_3": "value",
+            },
         ],
         "foo": "bar",
         "qux": 42,
@@ -161,8 +173,14 @@ fn unordered_doc_equality() {
 
     let d3 = unord_doc! {
         "key": [
-            1337,
-            null,
+            {
+                "inner_3": "value",
+                "inner_4": -42,
+            },
+            {
+                "inner_1": null,
+                "inner_2": 1337,
+            },
         ],
         "foo": "bar",
         "qux": 42,
@@ -180,4 +198,157 @@ fn unordered_doc_equality() {
 
     assert_doc_ne!(d2, d3);
     assert_doc_ne!(d3, d2);
+}
+
+#[test]
+fn unit_struct() {
+    #[derive(BsonSchema)]
+    struct FstUnit;
+
+    #[derive(BsonSchema)]
+    struct SndUnit();
+
+    let unit_schema = unord_doc! {
+        "type": ["array", "null"],
+        "maxItems": 0_i64,
+    };
+
+    let fst_schema = UnorderedDoc(FstUnit::bson_schema());
+    let snd_schema = UnorderedDoc(SndUnit::bson_schema());
+
+    assert_doc_eq!(fst_schema, snd_schema);
+    assert_doc_eq!(snd_schema, fst_schema);
+
+    assert_doc_eq!(fst_schema, unit_schema);
+    assert_doc_eq!(snd_schema, unit_schema);
+}
+
+#[test]
+fn newtype_struct() {
+    #[derive(BsonSchema)]
+    struct FloatingPoint(f64);
+
+    #[derive(BsonSchema)]
+    struct Angle(
+        #[magnet(min_incl = "-180", max_excl = "180")]
+        f32
+    );
+
+    assert_doc_eq!(
+        UnorderedDoc(FloatingPoint::bson_schema()),
+        UnorderedDoc(f64::bson_schema())
+    );
+
+    assert_doc_eq!(UnorderedDoc(Angle::bson_schema()), unord_doc! {
+        "type": "number",
+        "minimum": -180.0,
+        "exclusiveMinimum": false,
+        "maximum": 180.0,
+        "exclusiveMaximum": true,
+    });
+}
+
+#[test]
+fn tuple_struct() {
+    #[derive(BsonSchema)]
+    struct Complex(f64, f64);
+
+    #[derive(BsonSchema)]
+    struct IntRange(Option<u32>, Option<u32>);
+
+    assert_doc_eq!(UnorderedDoc(Complex::bson_schema()), unord_doc! {
+        "type": "array",
+        "additionalItems": false,
+        "items": [
+            { "type": "number" },
+            { "type": "number" },
+        ],
+    });
+
+    assert_doc_eq!(UnorderedDoc(IntRange::bson_schema()), unord_doc! {
+        "type": "array",
+        "additionalItems": false,
+        "items": [
+            {
+                "minimum": std::u32::MIN as i64,
+                "maximum": std::u32::MAX as i64,
+                "bsonType": ["int", "long", "null"],
+            },
+            {
+                "minimum": std::u32::MIN as i64,
+                "maximum": std::u32::MAX as i64,
+                "bsonType": ["int", "long", "null"],
+            },
+        ],
+    });
+}
+
+#[test]
+fn struct_with_named_fields() {
+    use std::collections::BTreeMap;
+
+    #[derive(BsonSchema)]
+    struct Contact {
+        names: Vec<String>,
+        address_lines: [String; 3],
+        phone_no: Option<u64>,
+        email: Option<Email>,
+        misc_info: Option<BTreeMap<String, String>>,
+    }
+
+    #[derive(Serialize, Deserialize, BsonSchema)]
+    #[serde(rename_all = "SCREAMING-KEBAB-CASE")]
+    struct Email {
+        #[serde(rename = "aDdReSs")]
+        address: String,
+        provider_name: String,
+    }
+
+    assert_doc_eq!(UnorderedDoc(Contact::bson_schema()), unord_doc! {
+        "type": "object",
+        "additionalProperties": false,
+        "required": [
+            "names",
+            "address_lines",
+            "phone_no",
+            "email",
+            "misc_info",
+        ],
+        "properties": {
+            "names": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                }
+            },
+            "address_lines": {
+                "type": "array",
+                "items": {
+                    "type": "string",
+                },
+                "minItems": 3 as i64,
+                "maxItems": 3 as i64,
+            },
+            "phone_no": {
+                "bsonType": ["int", "long", "null"],
+                "minimum": std::u64::MIN as i64,
+                "maximum": std::i64::MAX,
+            },
+            "email": {
+                "type": ["object", "null"],
+                "additionalProperties": false,
+                "required": ["aDdReSs", "PROVIDER-NAME"],
+                "properties": {
+                    "aDdReSs": { "type": "string" },
+                    "PROVIDER-NAME": { "type": "string" },
+                },
+            },
+            "misc_info": {
+                "type": ["object", "null"],
+                "additionalProperties": {
+                    "type": "string",
+                },
+            },
+        },
+    });
 }
